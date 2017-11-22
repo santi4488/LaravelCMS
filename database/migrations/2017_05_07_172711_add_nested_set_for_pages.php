b4b113aac32b891c519152b3db66586191f03239
santi4488 <csanti4442@yahoo.com>
2017-11-22 13:01:52 -0600

    fixed nested set error

--- database/migrations/2017_05_07_172711_add_nested_set_for_pages.php
+++ database/migrations/2017_05_07_172711_add_nested_set_for_pages.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
-use Kalnoy\Nestedset\Nestedset;
+use Kalnoy\Nestedset\NestedSet;
 
 class AddNestedSetForPages extends Migration
 {
