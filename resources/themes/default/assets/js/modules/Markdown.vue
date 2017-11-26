edde4d87855e1591b48e45e3454602240c35c879
santi4488 <csanti4442@yahoo.com>
2017-11-26 17:03:51 -0600

    made separate file structure for frontend and backend js

---
+++ resources/themes/default/assets/js/modules/Markdown.vue
@@ -0,0 +1,23 @@
+<template>
+
+</template>
+
+<script type="text/javascript">
+    export default {
+      props: {
+
+      },
+      created: function () {
+        
+      },
+      data () {
+          return {
+
+          }
+      },
+      methods: {
+
+      }
+
+    }
+</script>
